4379c2a79635b88211e5787bdfae2a786037a71d
Savindushehan <savindushehansamaraweera@gmail.com>
2023-04-17 21:09:59 +0530

    all upload

---
+++ JS/javascript.js
@@ -0,0 +1,19 @@
+console.clear();
+
+var EmailField=document.getElementById("email-field");
+var SubBTN=document.getElementById("submit-btn");
+var EmailError=document.getElementById("error");
+var pattern=/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
+
+SubBTN.onclick =function() {
+    if(!EmailField.value.match(pattern)){
+        EmailError.innerHTML="Please provide a valid email address";
+        EmailField.style.borderColor="hsl(354, 100%, 66%)";
+        return false;
+    }else{
+        EmailError.innerHTML="";
+        EmailField.style.borderColor=" hsl(0, 0%, 59%)";
+        return true;
+    }
+
+}
